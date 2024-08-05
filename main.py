d7dd62c73362c40a9cd00136e44a2d206c1cc2b6
raghuss4518 <133561609+raghuss4518@users.noreply.github.com>
2024-08-05 16:57:08 +0000

    My-First_day_learning-changes

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
-#print('Hello  wolrd')
+print('Hello  wolrd')
 
-#print('Hello My dear')
+print('Hello My dear')
 
-#print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
-#print("2. Knead the dough for 10 minutes.")
-#print("3. Add 3g of Salt.")
-#print("4. Leave to rise for 2 hours.")
-#print("5. Bake at 200 degrees C for 30 minutes.")
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
 
-#print("Hello World\n Hello world\n Hello Raghu")
+print("Hello World\n Hello world\n Hello Raghu")
 
-#print("HELLO" + " " + "RAMY")
+print("HELLO" + " " + "RAMY")
 
-#print("Day 1 - String Manipulation")
+print("Day 1 - String Manipulation")
 
-#print('String Concatenation is done with the "+" sign.')
+print('String Concatenation is done with the "+" sign.')
 
-#print('e.g. print("Hello " + "world")')
+print('e.g. print("Hello " + "world")')
 
-#print("New lines can be created with a backslash and n.")
+print("New lines can be created with a backslash and n.")
 
-#Input = input("What is your name? My name is ")
+Input = input("What is your name? My name is ")
 
-#print(Input)
-#name = "Raghu"
+print(Input)
+name = "Raghu"
 
-#print(name)
+print(name)
 
-#age = len(input('what about  your age'))
-#print(len)
+age = len(input('what about  your age'))
+print(len)
 
-#username = len(input('what about your score in python?'))
-#score = len(username)
-#print(username)
+username = len(input('what about your score in python?'))
+score = len(username)
+print(username)
 
 print("Welcome to the Band Name Generator.")
 city = input("which city did you grow up in?\n")
